de88ae22ddeb03335071a90d82aeacf8f94ad756
Anatoliy <ya.tolik-a@yandex.ru>
2017-08-14 12:42:39 +0300

    Наработки движка на PHP

---
+++ phprun/guest/login.php
@@ -0,0 +1,13 @@
+<? MessageShow();
+top('Вход');
+   
+ ?>
+
+<h1>Вход</h1>
+<form onsubmit="return false" >
+<p><input type="text" placeholder="E-mail" id="email"></p>
+<p><input type="password" placeholder="Пароль" id="password"></p>
+<p><input type="text" placeholder="<?captcha()?>" id="captcha"></p>
+<p><button onclick="post_query('gform', 'login', 'email.password.captcha')">Войти</button> <button onclick="go('recovery')">Восстановить пароль</button></p>
+</form>
+<? bottom() ?>
